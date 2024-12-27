b6da083a810d6ac18d3269e1602714ad474483d8
RealAriya <arya.t.innovate@gmail.com>
2024-12-27 19:56:25 +0330

    Edit website admin

--- website/admin.py
+++ website/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from website.models import Contact
 
 class ContactAdmin(admin.ModelAdmin):
-    list_display = ['subject','name','email','created_date','updated_date']
+    list_display = ['subject','first_name','email','created_date','updated_date']
     list_filter = ('subject',)
 
 
