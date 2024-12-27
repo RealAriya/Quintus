3dada40cb72868b347406619b593a8d76d087210
RealAriya <arya.t.innovate@gmail.com>
2024-12-27 19:16:47 +0330

    Create our Post, Category, Comment model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_lenght=255)
+
+    def __str__(self):
+        return self.name
+
+
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    # image
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    category = models.ManyToManyField(Category)
+    counted_view = models.IntegerField(default=0)
+    status = models.BooleanField(default=False)
+    published_date = models.DateTimeField(null=True)
+    login_required = models.BooleanField(default=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    class Meta():
+        ordering = ["created_date"]
+        verbose_name = "Post"
+    
+    def __str__(self):
+        return self.title
+    
+    def __add__(self, other):
+        if isinstance(other, int):
+            self.counted_view += other
+            self.save()
+        return self
+    
+    # def get_absolute_url(self):
+    #     return reverse('blog:single',kwargs={'pid':self.id})
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post,on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    subject = models.CharField(max_length=255)
+    email = models.EmailField()
+    message = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):          
+        return self.name
