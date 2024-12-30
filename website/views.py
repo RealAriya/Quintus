7639717f3349ec470623a9e0d831fcae54871b9a
RealAriya <arya.t.innovate@gmail.com>
2024-12-30 16:52:14 +0330

    How separate latest post and show it on website
    + Create a new html file (latest-post)
    + cut this part from index.html
    + create a posts view in views.py website
    + dynamic variables in template

--- website/views.py
+++ website/views.py
@@ -2,12 +2,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse,HttpResponseRedirect
 from website.models import Contact
+from blog.models import Post
+from django.utils import timezone
 from website.forms import Contact_Form
 from django.contrib import messages
 
 
 def index_view(request):
-    return render(request,'website/index.html')
+    now = timezone.now()
+    posts = Post.objects.filter(status = 1 , published_date__lte=now)[:3]
+
+    context = {'posts': posts,
+            }
+    
+    return render(request,'website/index.html',context)
 
 
 def about_view(request):
