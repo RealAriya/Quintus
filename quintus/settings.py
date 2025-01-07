64e86fc6fb9345b320e95202acef426ecd5c1256
RealAriya <arya.t.innovate@gmail.com>
2025-01-07 15:47:48 +0330

    Add Email config to reset password

--- quintus/settings.py
+++ quintus/settings.py
@@ -141,3 +141,20 @@ MEDIA_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
+]
+
+
+# Email.config
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'ariadjangoler@gmail.com'
+EMAIL_HOST_PASSWORD = 'mdceohkwreykuvav'
