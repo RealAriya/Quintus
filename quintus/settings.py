d98f723e7d86588764861285356cf6ac473ce5ba
RealAriya <arya.t.innovate@gmail.com>
2025-01-08 22:02:50 +0330

    Create env file and add some security headers
    + pip install python-decouple
    + add (from decouple import config) to settings
    + replace config(values) to real values
    + dedicate pictures to posts

--- quintus/settings.py
+++ quintus/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%1h3if%yknc%@7#)x()#-n(%py(o)7l$##$@(*j-r-7v11q9*5'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG",cast=bool,default=True)
 
 ALLOWED_HOSTS = []
 
@@ -86,11 +87,11 @@ WSGI_APPLICATION = 'quintus.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'Quintus',
-        'USER': 'David',
-        'PASSWORD': 'david11228',
-        'HOST': 'localhost',
-        'PORT': '5432', 
+        'NAME': config('DATABASE_NAME'),
+        'USER': config('DATABASE_USER'),
+        'PASSWORD': config('DATABASE_PASSWORD'),
+        'HOST': config('DATABASE_HOST'),
+        'PORT': config('DATABASE_PORT'), 
     }
 }
 
@@ -158,9 +159,31 @@ INTERNAL_IPS = [
 
 # Email.config
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'ariadjangoler@gmail.com'
-EMAIL_HOST_PASSWORD = 'mdceohkwreykuvav'
+EMAIL_BACKEND = config('EMAIL_BACKEND')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)  
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)  
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+
+
+if config('USE_SSL_SETTING',cast=bool,default=False):
+    ## X-Frame-Options
+    X_FRAME_OPTIONS = 'SAMEORIGIN'
+    #X-Content-Type-Options
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+    ## Strict-Transport-Security
+    SECURE_HSTS_SECONDS = 15768000
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
+
+    ## that requests over HTTP are redirected to HTTPS. aslo can config in webserver
+    SECURE_SSL_REDIRECT = True 
+
+    # for more security
+    CSRF_COOKIE_SECURE = True
+    CSRF_USE_SESSIONS = True
+    CSRF_COOKIE_HTTPONLY = True
+    SESSION_COOKIE_SECURE = True
+    SESSION_COOKIE_SAMESITE = 'Strict'
+    SECURE_SSL_REDIRECT = True
