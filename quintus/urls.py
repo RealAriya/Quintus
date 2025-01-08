aa9d7819b3f1a41acd6194afab105d0c9bda2719
RealAriya <arya.t.innovate@gmail.com>
2025-01-08 21:35:07 +0330

    Add Handlers
    + craete their views
    + add to urls.py main
    + add their templates

--- quintus/urls.py
+++ quintus/urls.py
@@ -6,6 +6,8 @@ from django.conf.urls.static import static
 from django.contrib.sitemaps.views import sitemap
 from website.sitemaps import StaticViewSitemap
 from blog.sitemaps import BlogSitemap
+from django.conf.urls import handler404, handler403, handler400, handler500
+from . import error_views
 
 sitemaps = {
     "static": StaticViewSitemap,
@@ -35,3 +37,10 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+# Set custom error handlers
+handler404 = error_views.handler404
+handler403 = error_views.handler403
+handler400 = error_views.handler400
+handler500 = error_views.handler500
